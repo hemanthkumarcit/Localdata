88209fdec2ca1498e1a7bdd53da200ce6dc17146
hemanthkumarcit <venkatanagakumar123@gmail.com>
2026-01-29 22:47:26 +0530

    changes done in file

--- weathertimeofday.py
+++ weathertimeofday.py
@@ -4,7 +4,7 @@ time_of_the_day = "night"
 if weather == "Snowy":
     print("you can play with your car toy")
 elif weather == "Rainy"
-    print("you can play with indoor games")
+    print("you can play with indoor games.")
 elif weather == "Sunny" and time_of_the_day == "night
     print("you can play with your teddy bear.")
 else:
